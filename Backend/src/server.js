f443ff95e1e1226ff6a3ef3e7570f6a0a570f439
Rads <radhika2changia@gmail.com>
2026-04-02 15:20:44 +0530

    deployment requirements

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from "./config/db.js";
@@ -9,7 +10,8 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 const app=express();
-const port=process.env.PORT;
+const port=process.env.PORT || 8080;
+const __dirname=path.resolve();
 
 await connectDB()
     .then(()=>{
@@ -26,7 +28,19 @@ await connectDB()
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(rateLimiter);
-app.use(cors({
-    origin: "http://localhost:5173"
-}))
+if(process.env.NODE_ENV!=="production")
+{
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }))
+}
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV==="production")
+{
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
+    })
+}
